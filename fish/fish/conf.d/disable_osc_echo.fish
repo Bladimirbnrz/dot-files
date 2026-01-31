9535d7e37078e1c32ad9d7d9c9c6247503987bf7
Bladimirbnrz <bladimirbnrz@gmail.com>
2026-01-31 16:04:27 -0600

    Delete fish directory

--- fish/fish/conf.d/disable_osc_echo.fish
+++
@@ -1,9 +0,0 @@
-# Evita que respuestas OSC/DA de terminal se impriman en la línea de comandos
-function __suppress_terminal_responses --on-event fish_prompt
-    stty -icanon -echo 2>/dev/null
-end
-
-function __restore_terminal_settings --on-event fish_postexec
-    stty icanon echo 2>/dev/null
-end
-
--- fish/functions/openRanger.fish
+++
@@ -1,6 +0,0 @@
-function openRanger
-    ranger --choosedir=$HOME/.rangerdir
-    if test -f $HOME/.rangerdir
-        cd (cat $HOME/.rangerdir)
-    end
-end
