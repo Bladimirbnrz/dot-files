82012b1e887b07ab885da50582dc77a7dedc0689
Bladimirbnrz <bladimirbnrz@gmail.com>
2026-02-02 19:05:34 -0600

    repair

--- fish/conf.d/disable_term_query.fish
+++
@@ -1,6 +0,0 @@
-# ~/.config/fish/conf.d/disable_query_term.fish
-# If the variable no-query-term is not present, it is automatically declared. 
-
-if not contains -- no-query-term $fish_features
-    set -Ua fish_features no-query-term
-end
